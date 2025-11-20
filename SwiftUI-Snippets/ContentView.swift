a99b1bc6b9ef46cec072b5b9719910b98b0b2cd5
Asim-IOS <m.asim@digitaldtech.com>
2025-11-20 17:12:40 +0500

    initial commit

---
+++ SwiftUI-Snippets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Snippets
+//
+//  Created by Digitaldtech on 20/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Snippets/SwiftUI_SnippetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SnippetsApp.swift
+//  SwiftUI-Snippets
+//
+//  Created by Digitaldtech on 20/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SnippetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
